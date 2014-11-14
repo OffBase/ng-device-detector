b92e54b52579dea1e3d2fa1b4f506f616b8cf824
Darren Dub <dardub@gmail.com>
2014-11-14 06:45:29 +0100

    Added playstation os and device

--- ng-device-detector.js
+++ ng-device-detector.js
@@ -16,6 +16,8 @@ angular.module("ng.deviceDetector",[])
     IPOD: "ipod",
     BLACKBERRY: "blackberry",
     FIREFOXOS: "firefoxos",
+    PS4: "ps4",
+    VITA: "vita",
     UNKNOWN: "unknown"
 })
 .constant("OS", {
@@ -26,6 +28,8 @@ angular.module("ng.deviceDetector",[])
     LINUX: "linux",
     UNIX: "unix",
     FIREFOXOS: "firefoxos",
+    PS4: "ps4",
+    VITA: 'vita',
     UNKNOWN: "unknown"
 })
 .service("detectUtils", ["deviceDetector", "DEVICES", "BROWSERS", "OS",
@@ -46,69 +50,76 @@ angular.module("ng.deviceDetector",[])
     }
 ])
 .factory("deviceDetector", ["$window", "DEVICES", "BROWSERS", "OS",
-	function ($window, DEVICES, BROWSERS, OS) {
-		var ua=$window.navigator.userAgent;
+  function ($window, DEVICES, BROWSERS, OS) {
+    var ua=$window.navigator.userAgent;
 
-		var deviceInfo = {
-		    raw: {
-		        userAgent: ua,
-				os:{
-					windows:/\bWindows\b/.test(ua),
-					mac:/\bMac OS\b/.test(ua),
-					ios:/\biPad\b/.test(ua) || /\biPhone\b/.test(ua) || /\biPod\b/.test(ua),
-					android:/\bAndroid\b/.test(ua),
-					linux:/\bLinux\b/.test(ua),
-					unix:/\bUNIX\b/.test(ua),
-					firefoxos:/\bFirefox\b/.test(ua) && /\Mobile\b/.test(ua)
-				},
-				browser:{
-					chrome:/\bChrome\b/.test(ua) || /\bCriOS\b/.test(ua),
-					firefox:/\Firefox\b/.test(ua),
-					safari:/^((?!CriOS).)*\Safari\b.*$/.test(ua),
-					opera:/\Opera\b/.test(ua),
-					ie:/\bMSIE\b/.test(ua) || /\Trident\b/.test(ua)
-				},
-				device:{
-					android:/\bAndroid\b/.test(ua),
-					ipad:/\biPad\b/.test(ua),
-					iphone:/\biPhone\b/.test(ua),
-					ipod:/\biPod\b/.test(ua),
-					blackberry:/\bblackberry\b/.test(ua),
-					firefoxos:/\bFirefox\b/.test(ua) && /\Mobile\b/.test(ua)
-				}
-			}
-		};
-
-	    deviceInfo.os = deviceInfo.raw.os.windows ? OS.WINDOWS :
-        	(deviceInfo.raw.os.ios ? OS.IOS :
-	        	(deviceInfo.raw.os.mac ? OS.MAC :
-		            (deviceInfo.raw.os.android ? OS.ANDROID :
-		                (deviceInfo.raw.os.unix ? OS.UNIX :
-		                    (deviceInfo.raw.os.linux ? OS.LINUX :
-		                        (deviceInfo.raw.os.firefoxos ? OS.FIREFOXOS : OS.UNKNOWN))))));
-	    deviceInfo.browser = deviceInfo.raw.browser.ie ? BROWSERS.IE :
-	        (deviceInfo.raw.browser.opera ? BROWSERS.OPERA :
-	            (deviceInfo.raw.browser.chrome ? BROWSERS.CHROME :
-	                (deviceInfo.raw.browser.firefox ? BROWSERS.FIREFOX :
-	                    (deviceInfo.raw.browser.safari ? BROWSERS.SAFARI : BROWSERS.UNKNOWN))));
-	    deviceInfo.device = deviceInfo.raw.device.android ? DEVICES.ANDROID :
-	        (deviceInfo.raw.device.iphone ? DEVICES.IPHONE :
-	            (deviceInfo.raw.device.ipad ? DEVICES.IPAD :
-		            (deviceInfo.raw.device.ipod ? DEVICES.IPOD :
-		                (deviceInfo.raw.device.blackberry ? DEVICES.BLACKBERRY :
-		                    (deviceInfo.raw.device.firefoxos ? DEVICES.FIREFOXOS : DEVICES.UNKNOWN)))));
-	    
-		return deviceInfo;
-	}
+    var deviceInfo = {
+        raw: {
+            userAgent: ua,
+        os:{
+          windows:/\bWindows\b/.test(ua),
+          mac:/\bMac OS\b/.test(ua),
+          ios:/\biPad\b/.test(ua) || /\biPhone\b/.test(ua) || /\biPod\b/.test(ua),
+          android:/\bAndroid\b/.test(ua),
+          linux:/\bLinux\b/.test(ua),
+          unix:/\bUNIX\b/.test(ua),
+          ps4:/\bPlaystation 4\b/.test(ua),
+          vita:/\bPlaystation Vita\b/.test(ua),
+          firefoxos:/\bFirefox\b/.test(ua) && /\Mobile\b/.test(ua)
+        },
+        browser:{
+          chrome:/\bChrome\b/.test(ua) || /\bCriOS\b/.test(ua),
+          firefox:/\Firefox\b/.test(ua),
+          safari:/^((?!CriOS).)*\Safari\b.*$/.test(ua),
+          opera:/\Opera\b/.test(ua),
+          ie:/\bMSIE\b/.test(ua) || /\Trident\b/.test(ua)
+        },
+        device:{
+          android:/\bAndroid\b/.test(ua),
+          ipad:/\biPad\b/.test(ua),
+          iphone:/\biPhone\b/.test(ua),
+          ipod:/\biPod\b/.test(ua),
+          blackberry:/\bblackberry\b/.test(ua),
+          ps4:/\bPlaystation 4\b/.test(ua),
+          vita:/\bPlaystation Vita\b/.test(ua),
+          firefoxos:/\bFirefox\b/.test(ua) && /\Mobile\b/.test(ua)
+        }
+      }
+    };
+      deviceInfo.os = deviceInfo.raw.os.windows ? OS.WINDOWS :
+          (deviceInfo.raw.os.ios ? OS.IOS :
+            (deviceInfo.raw.os.mac ? OS.MAC :
+                (deviceInfo.raw.os.android ? OS.ANDROID :
+                    (deviceInfo.raw.os.unix ? OS.UNIX :
+                        (deviceInfo.raw.os.linux ? OS.LINUX :
+                            (deviceInfo.raw.os.ps4 ? OS.PS4 :
+                                (deviceInfo.raw.os.vita ? OS.VITA :
+                                    (deviceInfo.raw.os.firefoxos ? OS.FIREFOXOS : OS.UNKNOWN))))))));
+      deviceInfo.browser = deviceInfo.raw.browser.ie ? BROWSERS.IE :
+          (deviceInfo.raw.browser.opera ? BROWSERS.OPERA :
+              (deviceInfo.raw.browser.chrome ? BROWSERS.CHROME :
+                  (deviceInfo.raw.browser.firefox ? BROWSERS.FIREFOX :
+                      (deviceInfo.raw.browser.safari ? BROWSERS.SAFARI : BROWSERS.UNKNOWN))));
+      deviceInfo.device = deviceInfo.raw.device.android ? DEVICES.ANDROID :
+          (deviceInfo.raw.device.iphone ? DEVICES.IPHONE :
+              (deviceInfo.raw.device.ipad ? DEVICES.IPAD :
+                (deviceInfo.raw.device.ipod ? DEVICES.IPOD :
+                    (deviceInfo.raw.device.blackberry ? DEVICES.BLACKBERRY :
+                        (deviceInfo.raw.device.ps4 ? DEVICES.PS4 :
+                            (deviceInfo.raw.device.vita ? DEVICES.VITA :
+                                (deviceInfo.raw.device.firefoxos ? DEVICES.FIREFOXOS : DEVICES.UNKNOWN)))))));
+          
+    return deviceInfo;
+  }
 ])
 .directive('deviceDetector', ["deviceDetector",function (deviceDetector) {
-	return {
-		restrict: "A",
-		link: function (scope, elm/*, attrs*/) {
-			elm.addClass('os-'+deviceDetector.os);
-			elm.addClass('browser-'+deviceDetector.browser);
-			elm.addClass('device-'+deviceDetector.device);
-		}
-	};
+  return {
+    restrict: "A",
+    link: function (scope, elm/*, attrs*/) {
+      elm.addClass('os-'+deviceDetector.os);
+      elm.addClass('browser-'+deviceDetector.browser);
+      elm.addClass('device-'+deviceDetector.device);
+    }
+  };
 }]);
 })(angular);
